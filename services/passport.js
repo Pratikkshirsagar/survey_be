7945daaf98803c555819e24ae9eb65f968a576ea
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-28 23:34:18 +0530

    authentication done.

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -16,9 +26,12 @@ passport.use(
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           // we already have a record of user
+          done(null, existingUser);
         } else {
           // we don't have the user record
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
         }
       });
     }
